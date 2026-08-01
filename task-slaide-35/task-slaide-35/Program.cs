1249fa1a4072e69bb10446b710a67fab1172c266
dem8ev <2301asdf@gmail.com>
2026-08-01 17:20:29 +0700

    Выполнил задание со слайда 35 (трехзначное число)

---
+++ task-slaide-35/task-slaide-35/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_slaide_35
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Объявление переменных
+            int n, a, b, c, s, p;
+            //Ввод данных:
+            Console.Write("Введите n = ");
+            n = Int32.Parse(Console.ReadLine());
+            //Блок решения:
+            a = n / 100; //Выделяем сотни
+            c = n % 10; //Выделяем единицы
+            b = (n / 10) % 10; //Выделяем десятки
+            p = a * b * c;
+            s = a + b + c;
+            //Вывод данных:
+            Console.WriteLine("Произведение цифр = {0}", p);
+            Console.WriteLine("Сумма цифр = {0}", s);
+        }
+    }
+}
