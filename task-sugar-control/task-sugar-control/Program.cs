8e0a0fec995e61e27f735445eb4941bdbd9bebf8
dem8ev <2301asdf@gmail.com>
2026-08-02 12:07:21 +0700

    Создал  программу, контролирующую потребление сахара (используя учловие if-else)

---
+++ task-sugar-control/task-sugar-control/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_sugar_control
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.Write("Введите количество ложек сахара, добавленных в чай a = ");
+            a = Int32.Parse(Console.ReadLine());
+            if (a == 0)
+            {
+                Console.WriteLine("Отлично! Вы супер, жирок на боках уходит эффективнее =)");
+            }
+            else
+            {
+                Console.WriteLine("Внимание, произошел выброс инсулина. Постарайтесь на прогулке идти быстрее!");
+            }
+        }
+    }
+}
