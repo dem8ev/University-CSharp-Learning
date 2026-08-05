f63bbeaae17a31d1e3810a7b895cb7a58d10c574
dem8ev <2301asdf@gmail.com>
2026-08-05 12:42:30 +0700

    window-schet-vozrasta: создал программу для подсчета возраста через  окно с полем ввода (используя VisualBasic)

---
+++ console-schet-vozrasta/console-schet-vozrasta/CodeFile1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualBasic;
+using System.Windows.Forms;
+class WindowAge
+{
+    static void Main()
+    {
+        //Текстовые переменные:
+        string res, txt;
+        //Целочисленные переменные:
+        int age, year = 2026, born;
+        //Отображение окна с полем ввода:
+        res = Interaction.InputBox("В каком году вы родились?", "Рассчет вашего возраста");
+        //Преобразование текста в число:
+        born = Int32.Parse(res);
+        //Вычисление возраста:
+        age = year - born;
+        txt = "Тогда ваш возраст равен " + age;
+        //Окно с сообщением:
+        MessageBox.Show(txt, "А вот и ваш возраст!");
+    }
+
+}
