5c901e2744ffa06f944a226c0d913b861b93ff52
dem8ev <2301asdf@gmail.com>
2026-08-05 10:53:46 +0700

    Создал программу, вызывающую окно для знакомства с пользователем (используя Microsoft.VisualBasic)

---
+++ learning-dialog-window/learning-dialog-window/CodeFile1.cs
@@ -0,0 +1,21 @@
+﻿//Использование ресурсов Visual Basic:
+using Microsoft.VisualBasic;
+using System.Windows.Forms;
+class InputDialogDemo
+{
+    //Главный метод программы:
+    static void Main()
+    {
+        //Текстовая переменная:
+        string name;
+        //Отображение окна с полем ввода:
+        name = Interaction.InputBox(
+            "Как вас зовут?",       //Текст над полем ввода
+            "Давайте знакомиться!"  //Название окна
+            );
+        //Еще одна текстовая переменная:
+        string txt = "Очень приятно," + name + "!";
+        //Окно с сообщением:
+        MessageBox.Show(txt, "Знакомство состоялось!");
+    }
+}
