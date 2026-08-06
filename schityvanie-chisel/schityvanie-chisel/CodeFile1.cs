a19579b5277786ce2c8708c195c396eb20b7af53
dem8ev <2301asdf@gmail.com>
2026-08-06 12:55:59 +0700

    shityvanie-chisel: Создал программу, считающую возраст в кносоли

---
+++ schityvanie-chisel/schityvanie-chisel/CodeFile1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class EnterInteger
+{
+    static void Main()
+    {
+        //Переменные
+        int a, b, year = 2026;
+        //Ввод данных:
+        Console.Write("В каком году вы родились a = ");
+        a = Int32.Parse(Console.ReadLine());
+        //Решение:
+        b = year - a;
+        //Вывод данных:
+        Console.Write("Вам " + b + "!");
+    }
+}
