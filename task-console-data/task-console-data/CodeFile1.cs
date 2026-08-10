c288c760a882b9d64b794e013e7755ec60f79be8
dem8ev <2301asdf@gmail.com>
2026-08-10 15:48:33 +0700

    Создал консольную программу для ввода и вывода данных о сегодняшнем дне

---
+++ task-console-data/task-console-data/CodeFile1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+class ConsoleData
+{
+    static void Main()
+    {
+        string DayOfTheWeek, month;
+        int date;
+        Console.Title = "Напоминание";
+        Console.WriteLine("Давайте выберем сегодняшнюю дату и установим напоминание!");
+        Console.Write("Укажите день недели: ");
+        DayOfTheWeek = Console.ReadLine();
+        Console.Write("Укажите название месяца: ");
+        month = Console.ReadLine();
+        Console.Write("Укажите номер этого дня в месце: ");
+        date = Int32.Parse(Console.ReadLine());
+        Console.WriteLine("Давайте убедимся, что данные введены верно:");
+        Console.WriteLine("День недели: " + DayOfTheWeek + ";");
+        Console.WriteLine("Название месяца: " + month + ";");
+        Console.WriteLine("Номер дня в месяце: " + date + ";");
+        Console.Write("Все верно?");
+    }
+}
