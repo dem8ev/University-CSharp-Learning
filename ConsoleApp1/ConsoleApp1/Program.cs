70c8cab7b26cd61e47d9b4a3c7657dde067440c3
dem8ev <2301asdf@gmail.com>
2026-08-11 13:20:26 +0700

    ВЫполнил задание 5 из лабораторной работы №1. (заменил int.Parse на double.Parse; в выводе значений у переменной b выбрал такое же форматирование, как и у переменной a, чтобы и у перменной b в случае чего перед разделительной точкой обозначался ноль (например, 0.46, а не .46)

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Объявление переменных
+            double x, y, z, a, b;
+            //Ввод данных (иницциализация переменных)
+            Console.Write("Введите x = ");  //вывод сообщения на экран
+            x = double.Parse(Console.ReadLine());  //ввод значения переменной с клавиатуры
+            Console.Write("Введите y = ");
+            y = double.Parse(Console.ReadLine());
+            Console.Write("Введите z = ");
+            z = double.Parse(Console.ReadLine());
+            //Реализация вычислений
+            a = Math.Cos(y - 3);
+            b = Math.Abs(x - y) * (1 + Math.Sin(z) / (x + y));
+            //Вывод значений
+            Console.WriteLine($"a = {a,5:#0.##}");
+            Console.Write($"b = {b,5:#0.##}");
+        }
+    }
+}
