8b138254c24a2e931bcbfdb3dc6b7eac560519fe
dem8ev <2301asdf@gmail.com>
2026-08-14 13:42:24 +0700

    lab1-task6.2: Создал конмольную программу самостоятельного задания №6.2 лабораторной работы №1 (про секунды после прошедших минут)

---
+++ lab1-task6.2/lab1-task6.2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1_task6._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N, minute = 60,
+                MinutesPassed,  //минут прошло
+                MinutesElapsedInSeconds,    //минут прошло в секундах
+                SecondsBeginningOfTheLastMinute;    //секунд прошло с последней минуты
+            Console.WriteLine("С начала суток прошло N секунд (N - целое). Найти количество секунд, прошедших с начала последней минуты.");
+            Console.Write("Введите количество секунд, прошедших с начала суток (целое число) N = ");
+            N = int.Parse(Console.ReadLine());
+            MinutesPassed = N / minute;
+            MinutesElapsedInSeconds = MinutesPassed * minute;
+            SecondsBeginningOfTheLastMinute = N - MinutesElapsedInSeconds;
+            //Строки с 20 - 22 можно объединить в одну так:
+            //SecondsBeginningOfTheLastMinute = N % minute;
+            //Либо без переменной minute:
+            //SecondsBeginningOfTheLastMinute = N % 60;
+            Console.WriteLine($"Количество секунд, прошедшее с начала последней минуты = {SecondsBeginningOfTheLastMinute} с.");
+        }
+    }
+}
