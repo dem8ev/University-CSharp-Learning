a713932abdf73fb12aed432f1a580673da34dda9
dem8ev <2301asdf@gmail.com>
2026-08-19 14:40:31 +0700

    lab2-example1: Создал программу, предназначенную для проверки введенного пользователем числа на четность/нечетность (использование тернарного оператора)

---
+++ lab2-example1/lab2-example1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2_example1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Целочисленные переменные
+            int number, reminder;
+            //Символьная переменная
+            string txt;
+            Console.Write("Введите целое число: ");
+            //Считывание целого числа:
+            number = int.Parse(Console.ReadLine());
+            //Вычисляем остаток от деления:
+            reminder = number % 2;
+            txt = (reminder == 0) ?"Введенное число четное!" : "Введенное число нечетное!";
+            Console.WriteLine(txt);
+        }
+    }
+}
