d7f4fcc25a1b2a0ac1acdff78f9474e6a8954571
dem8ev <2301asdf@gmail.com>
2026-08-19 14:56:26 +0700

    lab2-example2: программа для нахождения значения y, при двух произвольных действительных числах (a и b), используя условный оператор IF. Задание лаьоратоной работы

---
+++ lab2-example2/lab2-example2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2_example2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Действительные переменные:
+            double a, x, y;
+            //Считывание действительных чисел:
+            Console.Write("Введите действительное число x: ");
+            x = double.Parse(Console.ReadLine());
+            Console.Write("Введите действительное число y: ");
+            a = double.Parse(Console.ReadLine());
+            //Вычисляем значение переменной y:
+            if ((x >= 1) & (x <= 2))
+            {
+                y = Math.Tan(x);
+            }
+            else
+            {
+                if (x > 2)
+                {
+                    y = Math.Pow(a, x);
+                }
+                else
+                {
+                    y = x;
+                }
+            }
+            Console.WriteLine("y = {0,5:#0.###}", y);
+        }
+    }
+}
