fb305c73e8335c5af31d5b053e70e9a4daee8047
dem8ev <2301asdf@gmail.com>
2026-08-21 07:40:44 +0700

    lab2-example3: программа, находящая количество положительных чисел в исходном наборе (if + инкремент)

---
+++ lab2-example3/lab2-example3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2_example3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Целочисленные переменные:
+            int a, b, c, k;
+            Console.Write("Введите целое число a: ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Введите целое число b: ");
+            b = int.Parse(Console.ReadLine());
+            Console.Write("Введите целое число c: ");
+            c = int.Parse(Console.ReadLine());
+            k = 0;
+            if (a > 0) { k++; }
+            if (b > 0) { k++; }
+            if (c > 0) { k++; }
+            Console.WriteLine("Количество положительных чисел в исходном наборе = " + k);
+        }
+    }
+}
