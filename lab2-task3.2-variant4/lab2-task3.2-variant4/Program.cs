e57168f5bd1d6f6b7101848d9af4df6e57597ec7
dem8ev <2301asdf@gmail.com>
2026-08-24 13:33:54 +0700

    lab2-task3.2-variant4: программа для вычисления переменной y (условный оператор IF)

---
+++ lab2-task3.2-variant4/lab2-task3.2-variant4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2_task3._2_variant4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, b, y;
+            Console.WriteLine("Давайте вычислим, чему будет равна переменная y!");
+            Console.Write("Введите значение переменной x: ");
+            x = double.Parse(Console.ReadLine());
+            Console.Write("Введите значение переменной b: ");
+            b = double.Parse(Console.ReadLine());
+            if (x > 1.5)
+                y = Math.Pow(Math.Tan(x), 2) - Math.Cos(x) + Math.Sin(x);
+            else
+                if (x < -1)
+                    y = Math.Pow(x, 2) + (3 * Math.Pow(x, 3) / 4.0) + b;
+                else
+                    // Здесь не нужна строчка if (x >= -1 && x <= 1.5), ведь если число
+                    // не болше 1.5 и не меньше -1, то оно гарантированно находится в третьем
+                    // промежутке.
+                    // Если оставить строчку if (x >= -1 && x <= 1.5), то получится структура
+                    // if-else if-else if и компилятор не поймет, что три промежутка покрывают
+                    // всю числовую прямую
+                    y = Math.Sqrt(Math.Abs(Math.Cos(x)));
+            Console.WriteLine($"Переменная y = {y,5:#0.##}.");
+        }
+    }
+}
