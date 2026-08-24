4ccb43b7acc36e2965015cf027afd9b3b26a3f23
dem8ev <2301asdf@gmail.com>
2026-08-24 17:20:19 +0700

    lab2-task3.3.1: программа для проверки того, что 3 числа не превосходят n > 0 и кратны 5 (условный оператор IF)

---
+++ lab2-task3.3-variant4/lab2-task3.3-variant4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2_task3._3_variant4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, n;
+            Console.WriteLine("Из трех заданных целых чисел вывести те, что не превосходят заданного n>0\nи кратны 5.");
+            Console.Write("Введите целое число a = ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Введите целое число b = ");
+            b = int.Parse(Console.ReadLine());
+            Console.Write("Введите целое число c = ");
+            c = int.Parse(Console.ReadLine());
+            Console.Write("Введите заданное целое число n>0; n = ");
+            n = int.Parse(Console.ReadLine());
+            if (a <= n && a % 5 == 0)
+                Console.WriteLine(a);
+            if (b <= n && b % 5 == 0)
+                Console.WriteLine(b);
+            if (c <= n && c % 5 == 0)
+                Console.WriteLine(c);
+            if (!(a <= n && a % 5 == 0) && !(b <= n && b % 5 == 0) && !(c <= n && c % 5 == 0))
+                Console.WriteLine("Нет ни одного числа, удовлетворяющего начальным условиям.");
+        }
+    }
+}
