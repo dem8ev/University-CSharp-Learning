98dbe0d3b4340ce71b7692650cf4486313cc44d2
dem8ev <2301asdf@gmail.com>
2026-08-25 13:42:29 +0700

    lab2-task3.3.2-variant4: программа для магазина. Ввод стоимости книги, денег покупателя (условный оператор IF)

---
+++ lab2-task3.3.2-variant4/lab2-task3.3.2-variant4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2_task3._3._2_variant4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double BookValue, BuyersMoney, shortage, surplus;
+            Console.WriteLine("Приветствуем в нашем магазине!");
+            Console.Write("Введите стоимость выбранной книги (руб.): ");
+            BookValue = double.Parse(Console.ReadLine());
+            Console.Write("Введите сумму денег, которую вы внесли для покупки (руб.): ");
+            BuyersMoney = double.Parse(Console.ReadLine());
+            if (BuyersMoney < BookValue)
+            {
+                shortage = BookValue - BuyersMoney;
+                Console.WriteLine($"Добавьте {shortage,5:#0.##} руб.");
+            }
+            else
+                if (BuyersMoney > BookValue)
+                {
+                    surplus = BuyersMoney - BookValue;
+                    Console.WriteLine($"Возьмите сдачу {surplus,5:#0.##} руб.");
+                }
+                else
+                    Console.WriteLine("Спасибо");
+        }
+    }
+}
