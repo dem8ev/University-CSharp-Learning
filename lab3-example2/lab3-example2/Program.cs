3bfb5c64ece45d69834f41a7805800de6920479a
dem8ev <2301asdf@gmail.com>
2026-08-28 18:02:07 +0700

    lab3-example2: Даны два целых числа A и B. Найти cумму всех целых чисел от A до B включительно (цикл for)

---
+++ lab3-example2/lab3-example2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3_example2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Даны два целах числа A и B (A < B).
+            // Найти сумму всех целых чисел от A до B включительно.
+            int A, B, S;
+            Console.Write("Введите A = ");
+            A = int.Parse(Console.ReadLine());
+            Console.Write("Введите B = ");
+            B = int.Parse(Console.ReadLine());
+            S = 0;      // Сумму нужно занулить предед циклом, чтобы внутри цикла переменная суммы (S) не осталась не заданной
+            for (int i = A; i <= B; i++)
+            {
+                S += i;
+            }
+            Console.Write("Сумма всех целых чисел от A до B включительно = " + S);
+        }
+    }
+}
