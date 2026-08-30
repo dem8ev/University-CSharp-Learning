6990e56a081d4ba1e4367e8d985d6c277bc0cf92
dem8ev <2301asdf@gmail.com>
2026-08-30 12:24:41 +0700

    lab3-example5: таблица умножения (вложенный цикл for)

---
+++ lab3-example5/lab3-example5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3_example5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 9; i++)
+            {
+                for (int k = 1; k <= 9; k++)
+                {
+                    Console.Write($"{i * k,4}");
+                }
+                Console.WriteLine();
+            }    
+        }
+    }
+}
