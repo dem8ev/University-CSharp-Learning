a80ecac5cc764e4c56fad4c538b1ff02bc2fc988
dem8ev <2301asdf@gmail.com>
2026-08-30 12:07:27 +0700

    lab3-example4: пример из лаюораторной работы №4 (вложенный цикл: for {for})

---
+++ lab3-example4/lab3-example4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3_example4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int S, N;
+            S = 0; N = 0;
+            for (int i = 1; i <= 2; i++)
+            {
+                for (int k = 1; k <= 3; k++)
+                {
+                    S += i;
+                }
+                N += S;
+            }
+            Console.WriteLine("S = " + S);
+            Console.WriteLine("N = " + N);
+        }
+    }
+}
