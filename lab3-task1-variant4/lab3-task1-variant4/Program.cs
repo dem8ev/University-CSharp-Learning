e1143264d510b469dadaa362ddf71ab818a9e1f4
dem8ev <2301asdf@gmail.com>
2026-08-31 15:40:28 +0700

    lab3-task1-variant4: программа, рассчитывающая целые степени числа A (цикл for)

---
+++ lab3-task1-variant4/lab3-task1-variant4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3_task1_variant4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            double A;
+            Console.Write("Введите вещественное число A: ");
+            A = double.Parse(Console.ReadLine());
+            Console.Write("Введите целое число N (N > 0): ");
+            N = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= N; i++)
+            {
+                Console.WriteLine($"{Math.Pow(A, i):#0.##}");
+            }
+        }
+    }
+}
