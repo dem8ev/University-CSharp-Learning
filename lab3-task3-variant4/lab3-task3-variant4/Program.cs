79f0adf4069db15c12cb307577d6b6445dbf83f4
dem8ev <2301asdf@gmail.com>
2026-09-01 16:16:53 +0700

    lab3-task3-variant4: программа для суммирования доействительных чисел, согласно условию задачи (цикл for)

---
+++ lab3-task3-variant4/lab3-task3-variant4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3_task3_variant4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int NumbersOfTerms = 5;
+            double S, volume;
+            S = 0;
+            Console.WriteLine("Получение суммы чисел, удовлетворяющих условию задачи:\n\nВведите последовательно 5 действительных чисел a1, a2, ... a5:\n");
+            for (int i = 1; i <= NumbersOfTerms; i++)
+            {
+                Console.Write($"Введите {i}-е действительное число: ");
+                volume = double.Parse(Console.ReadLine());
+                if (Math.Abs(volume) < Math.Pow(i, 2))
+                {
+                    S += volume;
+                }
+            }
+            Console.WriteLine($"Сумма всех действительных чисел, удовлетворяющих условию задачи = {S,5:##.##}");
+        }
+    }
+}
