c879d0ea24f5acec2fd982ccf3d1e42d2e4fad9d
dem8ev <2301asdf@gmail.com>
2026-09-01 16:43:43 +0700

    lab3-task4-variant4: программа, выводящая значения согласно условию задачи (вложенный цикл for)

---
+++ lab3-task4-variant4/lab3-task4-variant4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3_task4_variant4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int y;
+            y = 0;
+            for (int c = 2; c <= 4; c++)
+            {
+                for (int k = -1; k <= 1; k++)
+                {
+                    y += (int)Math.Pow(c * k, 2);
+                }
+            }
+            Console.WriteLine($"Значение выражения из условия задачи: y = {y}");
+        }
+    }
+}
