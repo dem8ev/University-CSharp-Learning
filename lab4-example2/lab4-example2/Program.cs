0e747fa609eddaeaaf2fc7e81060db3e326deb3d
dem8ev <2301asdf@gmail.com>
2026-09-02 12:37:40 +0700

    lab4-example3: программа для расчета значений функции из условия (цикл while)

---
+++ lab4-example2/lab4-example2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4_example2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, y, dx;
+            Console.Write("Укажите начальное значение a = ");
+            a = double.Parse(Console.ReadLine());
+            Console.Write("Укажите конечное значение b = ");
+            b = double.Parse(Console.ReadLine());
+            Console.Write("Укажите значение шага табулирования dx = ");
+            dx = double.Parse(Console.ReadLine());
+            while (a <= b)
+            {
+                y = 1.0 / (a + 1);
+                Console.WriteLine($"При значении x = {a:F2} y = {y:F3}");
+                a += dx;
+            }
+        }
+    }
+}
