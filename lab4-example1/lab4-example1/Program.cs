e87a4fc1cb4d53b34a61f4bb755dca35c6ff90ea
dem8ev <2301asdf@gmail.com>
2026-09-02 12:14:53 +0700

    lab4-example1: программа для вычисления количества цифр в числе (цикл while)

---
+++ lab4-example1/lab4-example1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4_example1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            int count;
+            Console.Write("Введите уелое число N > 0, N = ");
+            N = int.Parse(Console.ReadLine());
+            count = 0;
+            while (N != 0)
+            {
+                N = N / 10;
+                count += 1;
+            }
+            Console.WriteLine($"Количество чисел в указанном числе = {count}.");
+        }
+    }
+}
