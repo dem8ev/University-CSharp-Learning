b5bd178e993d011e19529ec0903352d0e78e520a
dem8ev <2301asdf@gmail.com>
2026-09-02 13:03:32 +0700

    lab4-example4: проверка числа (является ли оно степенью числа 3) (цикл while)

---
+++ lab4-example4/lab4-example4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4_example4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N, P;
+            Console.Write("Целое число N (N > 0) = ");
+            N = int.Parse(Console.ReadLine());
+            P = 1;
+            while (P < N)
+                P *= 3;
+            Console.WriteLine(N == P);
+        }
+    }
+}
