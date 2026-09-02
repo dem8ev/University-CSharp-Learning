151ee98047f8f4affd487e544129c0313b017fc1
dem8ev <2301asdf@gmail.com>
2026-09-02 12:49:56 +0700

    lab4-example3: расчет незанятой части отрезка A (цикл while)

---
+++ lab4-example3/lab4-example3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4_example3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double A, B;
+            Console.Write("Введите длину отрезка A = ");
+            A = double.Parse(Console.ReadLine());
+            Console.Write("Введите длину отрезка B (B < A) = ");
+            B = double.Parse(Console.ReadLine());
+            while (A >= B)
+                A -= B;
+            Console.WriteLine($"Длина незанятой части отрезка A = {A}.");
+        }
+    }
+}
