8296c0ce087b8f0b46279e9eaab288f794836ddd
dem8ev <2301asdf@gmail.com>
2026-09-08 17:47:22 +0700

    lab4-task2.2-variant4: программа для проверки присутствия "2" в числе (цикл do-while)

---
+++ lab4-task2.2-variant4/lab4-task2.2-variant4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4_task2._2_variant4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            long N, rest;   //long для тогго, чтобы пользователь смог ввести прям большое число
+            Console.Write("Введите целое число N (N > 0): ");
+            N = long.Parse(Console.ReadLine());
+            do
+            {
+                rest = N % 10;
+                N /= 10;
+            }
+            while (N > 0 && rest != 2);
+            Console.WriteLine(rest == 2);
+        }
+    }
+}
