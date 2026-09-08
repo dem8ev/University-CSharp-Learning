3b7d44e3d25655a81ae2c56e86079e33aec8e529
dem8ev <2301asdf@gmail.com>
2026-09-08 13:43:36 +0700

    lab4-task1-while-variant4: рассчет значений функции (способ - цикл while)

---
+++ lab4-task1-while-variant4/lab4-task1-while-variant4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace lab4_task1_while_variant4
+{
+    internal class While
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            double y, x = 0.6, MaxInterval = 1.0, dx = 0.1;
+            Console.Write("Введите натуральное число a = ");
+            a = int.Parse(Console.ReadLine());
+            while (x <= MaxInterval)
+            {
+                y = Math.Tan(x) * Math.Sqrt(1 + a * Math.Pow(x, 2));
+                Console.WriteLine($"При значении x = {x:F1} y = {y:F3}");
+                x += dx;
+            }
+        }
+    }
+}
---
+++ lab4-task1-while-variant4/lab4-task1-while-variant4/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("lab4-task1-while-variant4")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("lab4-task1-while-variant4")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("1df158da-5d64-4d44-a9c3-f6af898d3973")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
