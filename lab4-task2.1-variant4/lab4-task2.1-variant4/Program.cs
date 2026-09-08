16d7f5e0b072a1b5c4b42d25d626981024b943b3
dem8ev <2301asdf@gmail.com>
2026-09-08 15:27:04 +0700

    lab4-task2.1-variant4: задача из самостоятельной работы (цикл while)

---
+++ lab4-task2.1-variant4/lab4-task2.1-variant4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4_task2._1_variant4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double A, Sum;
+            int K;
+            Console.Write("Введите число A (A > 1): ");
+            A = double.Parse(Console.ReadLine());
+            Sum = 1.0;
+            K = 1;
+            while (Sum <= A)
+            {
+                K++;
+                Sum += 1.0 / K;
+            }
+            Console.WriteLine($"Наименьшее из целых чисел K = {K}, сама эта сумма = {Sum}");
+        }
+    }
+}
